4edd718b87f9894f8a0bdadc970ff034418cb0ac
Vegard <vegarend@noreply.no>
2020-03-20 17:30:12 +0100

    Some more options in yaml

--- b2batch/b2batch.py
+++ b2batch/b2batch.py
@@ -3,6 +3,7 @@
 import yaml
 import argparse
 import subprocess
+import sys
 
 parser = argparse.ArgumentParser(description='Argparse example')
 parser.add_argument('-y','--yaml', help='Some YAML file', required=True)
@@ -11,18 +12,20 @@ args = parser.parse_args()
 with open(args.yaml, 'r') as f:
   doc = yaml.safe_load(f)
 
+if doc['bucket'] == None:
+  sys.exit(0)
 
 for item in doc['localhost']:
-  cmd = "b2 sync --excludeAllSymlinks {} b2://nasse-primary{}".format(item,item)
-  #cmd = "b2 sync --delete --excludeAllSymlinks {} b2://nasse-primary{}".format(item,item)
-  print(cmd)
-  subprocess.run(cmd, shell=True)
+  bucket = doc['bucket']
+  if doc['delete']:
+    cmd = "b2 sync --delete --excludeAllSymlinks {} b2://{}{}".format(item,bucket,item)
+  else
+    cmd = "b2 sync --excludeAllSymlinks {} b2://{}{}".format(item,bucket,item)
+  
+  if doc['sync'] 
+    print("Executing {}".format(cmd))
+    subprocess.run(cmd, shell=True)
+  else:
+    print("Test run: {}".format(cmd))
 
 print("Done!")
-
-#b2 sync --excludeAllSymlinks ${dir} b2://nasse-primary/${dir}
-#b2 sync --excludeAllSymlinks --delete test2 b2://nasse-test/atest
-
-#subprocess.run(["ls", "-l"])
-#subprocess.run(["ls", "-l"])
-
